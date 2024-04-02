ea8bbd55a6139476e07c421520cddef812dc093e
Adel Ansari <adelansari.a@gmail.com>
2024-04-03 00:38:32 +0300

    Add file creation functionality and updating in convex db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@
 
 import { Button } from '@/components/ui/button';
 import { SignInButton, SignOutButton, SignedIn, SignedOut, useSession } from '@clerk/nextjs';
+import { useMutation } from 'convex/react';
+import { api } from '../../convex/_generated/api';
 
 export default function Home() {
   const session = useSession();
+  const createFile = useMutation(api.files.createFile);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <SignedIn>
@@ -17,6 +20,16 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
+
+      <Button
+        onClick={() => {
+          createFile({
+            name: 'Hello World',
+          });
+        }}
+      >
+        Click Me
+      </Button>
     </main>
   );
 }
